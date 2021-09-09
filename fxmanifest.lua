019ab3df4d4301551be397f5580379a66c4d0044
Coykn3 <53879234+coykn3@users.noreply.github.com>
2021-09-09 20:19:26 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'adamant'
 games { 'gta5' };
 client_scripts {
-        'lua/cl.lua',
+        'cl.lua',
 }
 server_scripts {
         '@mysql-async/lib/MySQL.lua',
         'Config.lua',
-        'lua/sv.lua',
+        'sv.lua',
 }
