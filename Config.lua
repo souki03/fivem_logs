ce7f136732f1b54ceeae1ccbaae6421f3f14522c
Coykn3 <53879234+coykn3@users.noreply.github.com>
2021-09-09 20:15:38 +0200

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,27 @@
+Webhook = {
+    Name = "coykn3_logs",
+    Logo = "https://media.discordapp.net/attachments/743953072266412056/744662695541735504/CoreoLogo.png",
+    ShowIp = true,
+    Chan = {
+        -- Base --
+        ["State"] = "yourwebhooklink",
+        ["Connnection"] = "yourwebhooklink",
+        ["Kill"] = "yourwebhooklink",
+        ["Item"] = "yourwebhooklink",
+        ["Cash"] = "yourwebhooklink",
+        ["Weapon"] = "yourwebhooklink",
+        ["Car"] = "yourwebhooklink",
+        -- Other --
+        ["Staff"] = "yourwebhooklink",
+        ["Illegal"] = "yourwebhooklink",
+        ["Job"] = "yourwebhooklink",
+    },
+    Color = {
+        ["Blue"] = 3447003,
+        ["Red"] = 15158332,
+        ["Green"] = 3066993,
+        ["Yellow"] = 15844367,
+        ["Grey"] = 9807270,
+        ["Purple"] = 10181046,
+    },
+}
